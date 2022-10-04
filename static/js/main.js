ed6e11d949a08d94baef69b90fbc33be0e349825
Dannika <71364766+dannikate@users.noreply.github.com>
2022-10-04 11:53:50 +1000

    created Leaflet map with different base maps

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,52 @@
 /* TODO
 1) Create Leaflet map from JSON file--coordinates only. 
 */ 
+
+const map = L.map('map', {
+    center: [13.4443, 144.7937],
+    zoom: 11,
+})
+
+const baseLayersZoom = 19;
+
+// Open Street Map layer 
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: baseLayersZoom, 
+    attribution: '© OpenStreetMap'
+}).addTo(map)
+
+// ESRI World Street Map 
+const ewsp = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: baseLayersZoom,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+})
+
+// ESRI World Topo Map 
+const ewtm = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: baseLayersZoom, 
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+});
+
+// ESRI World Imagery 
+const ewi = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: baseLayersZoom,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}); 
+
+// ESRI World Shaded Relief 
+const ewsr = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: baseLayersZoom,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri'
+});
+
+
+const baseLayers = {
+    'Open Street Map': osm,
+    'ESRI World Street Map': ewsp,
+    'ESRI World Topo Map': ewtm,
+    'ESRI World Imagery': ewi,
+    'ESRI World Shaded Relief': ewsr
+}
+
+const layerControl = L.control.layers(baseLayers).addTo(map)
+
