a1fd3ab3920c41d6287aa4c61cbc84d6fe1528b5
Dannika <71364766+dannikate@users.noreply.github.com>
2022-10-04 11:51:02 +1000

    added JS and JSON files with updated data structure

---
+++ static/data/data1.js
@@ -0,0 +1,119 @@
+export default [
+    {
+    Well: "A-1",
+    Location: {
+        Lat: 13.45409,
+        Lon: 144.7594,
+        Basin: "Hagatna"
+        },
+    Stats: {
+        Average: 1.47490566,
+        Min: 0.65,
+        Max: 3.1,
+        Mode: 1.3,
+        Slope: 0.0000210396,
+        Intercept: 0.717126208,
+        Std_Dev: 0.421816687,
+        Deg_of_Free: 51,
+        Rcrit: 0.2706,
+        Rcalc_Mo: 0.413461739,
+        Rcalc_New: 0.181079821,
+        EA: 0.414843384,
+        EA_X2: 0.829686767,
+        Base_Year: "1/1/86",
+        End_Year: "1/1/20",
+        Top1: 0.829686767,
+        Top2: 0.829686767,
+        Bottom1: 0.963200948,
+        Bottom2: 1.224471162,
+        Inc_10_Yrs: 0.076794676,
+        Inc_20_Yrs: 0.153589352,
+        X_Yrs_1ppm: 130.2173607,
+        Sig: 0,
+        MoP: 0,
+        Annual_Freq: 1.261904762
+        },
+    Plot: {
+        X: [2015, 2016, 2017, 2018, 2019, 2020],
+        Y: [1, 2, 3, 4, 5, 6]
+        }
+    },
+    {
+    Well: "A-2",
+    Location: {
+        Lat: 13.44534,
+        Lon: 144.77608,
+        Basin: "Hagatna"
+        },
+    Stats: {
+        Average: 2.88,
+        Min: 0.72,
+        Max: 3.96,
+        Mode: 2.7,
+        Slope: 0.000138721,
+        Intercept: -1.97449894,
+        Std_Dev: 0.614201496,
+        Deg_of_Free: 45,
+        Rcrit: 0.2876,
+        Rcalc_Mo: 0.27805893,
+        Rcalc_New: 0.600707786,
+        EA: 0.491034854,
+        EA_X2: 0.982069708,
+        Base_Year: "1/1/86",
+        End_Year: "1/1/20",
+        Top1: 0.982069708,
+        Top2: 0.982069708,
+        Bottom1: 1.892105312,
+        Bottom2: 3.61474124,
+        Inc_10_Yrs: 0.506331224,
+        Inc_20_Yrs: 1.012662448,
+        X_Yrs_1ppm: 19.7499177,
+        Sig: 1,
+        MoP: 1,
+        Annual_Freq: 1.119047619
+        },
+    Plot: {
+        X: [2015, 2016, 2017, 2018, 2019, 2020],
+        Y: [7, 8, 9, 10, 11, 12]
+        }
+    },
+    {
+    Well: "A-3",
+    Location: {
+        Lat: 13.45409,
+        Lon: 144.7594,
+        Basin: "Hagatna"
+        },
+    Stats: {
+        Average: 1.47490566,
+        Min: 0.65,
+        Max: 3.1,
+        Mode: 1.3,
+        Slope: 0.0000210396,
+        Intercept: 0.717126208,
+        Std_Dev: 0.421816687,
+        Deg_of_Free: 51,
+        Rcrit: 0.2706,
+        Rcalc_Mo: 0.413461739,
+        Rcalc_New: 0.181079821,
+        EA: 0.414843384,
+        EA_X2: 0.829686767,
+        Base_Year: "1/1/86",
+        End_Year: "1/1/20",
+        Top1: 0.829686767,
+        Top2: 0.829686767,
+        Bottom1: 0.963200948,
+        Bottom2: 1.224471162,
+        Inc_10_Yrs: 0.076794676,
+        Inc_20_Yrs: 0.153589352,
+        X_Yrs_1ppm: 130.2173607,
+        Sig: 0,
+        MoP: 0,
+        Annual_Freq: 1.261904762
+        },
+    Plot: {
+        X: [2015, 2016, 2017, 2018, 2019, 2020],
+        Y: [18, 17, 16, 15, 14, 13]
+        }
+    }
+]
