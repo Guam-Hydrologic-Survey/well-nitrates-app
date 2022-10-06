4c2c0bb0f3a578a76aa37d7741148bb21fa93fb0
Dannika <71364766+dannikate@users.noreply.github.com>
2022-10-06 12:58:54 +1000

    working plot function with dummy data; still need to link up actual plot data from selected when upon click

--- static/js/main.js
+++ static/js/main.js
@@ -46,35 +46,52 @@ const baseLayers = {
 
 const layerControl = L.control.layers(baseLayers).addTo(map)
 
+// Array to house well markers on the map
+var wellMarkers = [];
+var wellPlot = [];
+
 // Gets the data from the JSON file and adds well to the map
 fetch('./static/data/data3.json')
     .then(response => response.json())
     .then(data => {
         // For loop traverses through each well in JSON file, extracts coordinates and adds points to the map
         for (const well of data.wells) {
-            console.log(well.Name + ', Coords: [' + well.Location.Lat + ', ' + well.Location.Lon + ']');
+            // console.log(well.Name + ', Coords: [' + well.Location.Lat + ', ' + well.Location.Lon + ']');
             var latLng = L.latLng(well.Location.Lat, well.Location.Lon);
-            // console.log(latLng);
-            L.marker(latLng).addTo(map)
+            console.log(latLng);
+            // L.marker(latLng).addTo(map)
+            var marker = new L.marker(latLng)
+            marker.addTo(map)
                 .bindPopup(
                     `Well: ${well.Name} <br> Lat: ${well.Location.Lat} <br> Lon: ${well.Location.Lon} <br> 
-                    <button type="button" class="btn" data-bs-toggle="modal" onclick="">Plot</button>`
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" onclick="plotWNL()" data-bs-target="#exampleModal">Plot</button>`
                 );
+            wellMarkers.push(marker);
+            
             // Prints [x,y] to console--a check to ensure data from each well is correct 
-            console.log(`x: ${well.Plot.X}, y: ${well.Plot.Y} \n\n`)
+            // console.log(`x: ${well.Plot.X}, y: ${well.Plot.Y} \n\n`)
         }
+        // JSON is local; only available through this fetch function, so need to pass data 
+        // console.log(JSON.stringify(data))
+
     })
     .catch(console.error);
+// console.log(wellMarkers)
+
+// Used to test functionality of Plot button 
+function showMessage() {
+    alert('Clicked');
+}
 
 let plotData 
 
 // Plots data points from selected well to chart 
 // TODO: Function that will pass plot data based on selected well on map 
-const plotWNL = (plotData, selectedWell) => {
+const plotWNL = (selectedWell) => {
     // Plots x,y coordinates 
     const wnlTrace = {
-        // x: ,
-        // y: ,
+        x: [2015,2016,2017,2018,2019,2020],
+        y: [1,2,3,4,5,6],
         type: 'scatter', 
         mode: 'markers',
         name: 'Well Nitrate Levels'
