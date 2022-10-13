fe8611b4e6e8af590565fd8b8ef77eb25729823a
Dannika <71364766+dannikate@users.noreply.github.com>
2022-10-13 17:59:46 +1000

    resolved merge conflicts

---
+++ static/js/main.js
@@ -0,0 +1,166 @@
+// Open Street Map layer 
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: baseLayersZoom, 
+    attribution: '© OpenStreetMap'
+}).addTo(map)
+
+// ESRI World Street Map 
+const ewsp = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: baseLayersZoom,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+})
+
+// ESRI World Topo Map 
+const ewtm = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: baseLayersZoom, 
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+});
+
+// ESRI World Imagery 
+const ewi = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: baseLayersZoom,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}); 
+
+// ESRI World Shaded Relief 
+const ewsr = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: baseLayersZoom,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri'
+});
+
+
+const baseLayers = {
+    'Open Street Map': osm,
+    'ESRI World Street Map': ewsp,
+    'ESRI World Topo Map': ewtm,
+    'ESRI World Imagery': ewi,
+    'ESRI World Shaded Relief': ewsr
+}
+
+const layerControl = L.control.layers(baseLayers).addTo(map);
+
+// Plots data points from selected well to chart 
+let plotData 
+const plotWNL = () => {
+
+    // Array to hold date objects
+    const x_dates_conv = [];
+
+    // Converted date strings from x_vals to JS date objects 
+    for (let i = 0; i < plotData.x_vals.length; i++) {
+        x_dates_conv[i] = new Date(plotData.x_vals[i]);
+        console.log(`${plotData.x_vals[i]} --> ${x_dates_conv[i]}`);
+    }
+
+    // Plots x,y coordinates 
+    const wnlTrace = {
+        x: x_dates_conv,
+        y: plotData.y_vals,
+        type: 'scatter', 
+        mode: 'markers',
+        name: 'Well Nitrate Levels'
+    }
+
+    // Plot features and layout
+    const layout = {
+        title: {
+            text: `Nitrate Levels for Well ${plotData.name}`,
+            font: {
+                size: 20
+            }
+        },
+        xaxis: {
+            title: 'Years'
+        },
+        yaxis: {
+            title: 'ppm (mg/L)'
+        }
+    }
+    Plotly.newPlot('plot', [wnlTrace], layout, {scrollZoom: true})
+}
+
+let getStats
+const showStats = () => {
+    document.getElementById("sidebar").innerHTML =
+        `
+            <div>
+                <h4>Well ${getStats.name}</h4>
+                <p class="stats-location">${getStats.lat}, ${getStats.lon}</p>
+                <p class="stats-location">${getStats.basin} Basin</p>
+                <hr/>
+            </div>
+
+            <div class="stats-row">
+                <div class="stats-col">
+                    <p class="stats-text">Average</p>
+                    <p class="stats-text">Min</p>
+                    <p class="stats-text">Max</p>
+                    <p class="stats-text">Mode</p>
+                    <p class="stats-text">Slope</p>
+                    <p class="stats-text">Intercept</p>
+                    <p class="stats-text">Standard Deviation</p>
+                    <p class="stats-text">Degrees of Freedom</p>
+                    <br>
+                    <br>
+                </div>
+                <div class="stats-col">
+                    <p class="stats-num">${getStats.average}</p>
+                    <p class="stats-num">${getStats.min}</p>
+                    <p class="stats-num">${getStats.max}</p>
+                    <p class="stats-num">${getStats.mode}</p>
+                    <p class="stats-num">${getStats.slope}</p>
+                    <p class="stats-num">${getStats.intercept}</p>
+                    <p class="stats-num">${getStats.std_dev}</p>
+                    <p class="stats-num">${getStats.deg_of_free}</p>
+                    <br>
+                    <button type="button" class="btn btn-primary">View Full Statistics</button>
+                </div>
+            </div>
+        `
+}
+
+// Filepaths for map (lat, lon coords) json and data (stats, x-y vals) json 
+const data_url = './static/data/data3.json';
+const map_url = './static/data/data4.json'; 
+
+// Testing single marker to trigger on click event
+L.marker([13.45409, 144.7594]).addTo(map).on('click', function(e) {
+    console.log(e.latlng);
+});
+
+// Gets the data from the JSON file and adds well to the map
+fetch(map_url)
+    .then(response => response.json())  // Requests for a json file as a response
+    .then(geojson => { 
+
+        // Creates pop-ups for each point on the map 
+        const getWellInfo = (feature, layer) => {
+            layer.bindPopup(
+                `
+                <strong>Well</strong>: ${feature.properties.name} 
+                <br><strong>Lat:</strong> ${feature.properties.lat} 
+                <br><strong>Lon:</strong> ${feature.properties.lon}
+                <br><strong>Basin:</strong> ${feature.properties.basin}
+                <br><br>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" onclick="plotWNL()" data-bs-target="#exampleModal">Plot</button>
+                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" onclick="showStats()">Statistics</button>
+                `
+            );
+
+            // On click event on the points
+            // Sends data for clicked item to global variable plotData 
+            layer.on('click', pt => plotData = pt.target.feature.properties) 
+            layer.on('click', pt => getStats = pt.target.feature.properties)
+        }
+
+        // Places points on the map and calls on getWellInfo function (right above) to show pop-ups 
+        const mapJson = L.geoJSON(geojson, {onEachFeature: getWellInfo}).addTo(map);
+        layerControl.addOverlay(mapJson, "Wells") 
+    })
+    .catch(console.error);
+
+// Used to test functionality of Plot button 
+function showMessage(name) {
+    console.log(`Clicked on Statistics btn`);
+    alert(`Clicked on Statistics btn`);
+}
