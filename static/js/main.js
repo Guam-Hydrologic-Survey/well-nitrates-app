571be3852d8df69acb9ba39e62c11a144c0f12ae
Dannika <71364766+dannikate@users.noreply.github.com>
2022-10-17 10:24:03 +1000

    removed highlight for view full stats accordion on collapse

--- static/js/main.js
+++ static/js/main.js
@@ -127,7 +127,7 @@ const showStats = () => {
             <div class="accordion" id="accordionExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                         View Full ${getStats.name} Statistics
                     </button>
                     </h2>
